9fb28248ec11cd55f421b00b85151276b0ac1869
gonzalo olivares <g.olivaresca@gmail.com>
2024-04-08 17:07:07 -0400

    commit desafio

---
+++ laesquina/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/golivares/proyectosDjango/desafio_entorno_virtual/laesquina/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
